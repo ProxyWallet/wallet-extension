0fce501b40dcf7dba7abafe0965bbf12639e561c
Danil Dovgal <91081974+qweblessed@users.noreply.github.com>
2022-09-30 13:14:04 +0300

    merge fix

--- packages/extension/src/Content/content.ts
+++ packages/extension/src/Content/content.ts
@@ -3,17 +3,29 @@ import browser from "webextension-polyfill";
 
 function injectScript() {
   try {
+    console.log('inject script');
     const injectURL = browser.runtime.getURL("inject.bundle.js");
+    console.log('url', injectURL)
+    console.log('inject script2');
+
     const container = document.head || document.documentElement;
     const scriptTag = document.createElement("script");
     scriptTag.setAttribute("async", "false");
     scriptTag.src = `${injectURL}`;
-    scriptTag.id = 'enkrypt-inject'
-    scriptTag.onload = function () {
-      console.info("Hello from the content-script");
-      container.removeChild(scriptTag);
-    };
+    scriptTag.id = 'undas-inject'
+    console.log('inject script21');
+
+    // scriptTag.onload = function () {
+    // console.log('onload');
+      
+    //   console.info("Hello from the content-script");
+    //   container.removeChild(scriptTag);
+    // };
+    console.log('inject script3');
+
     container.insertBefore(scriptTag, container.children[0]);
+    console.log('inject script4');
+
   } catch (error) {
     console.error("Undas: Provider injection failed.", error);
   }
